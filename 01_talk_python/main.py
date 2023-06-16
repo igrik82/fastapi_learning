30f53fe33fff7151e64d5d48fd33f333e735daa0
Igor <igrik82@gmail.com>
2023-06-16 10:51:35 +0500

    03-the-most-basic-api

--- 01_talk_python/main.py
+++ 01_talk_python/main.py
@@ -1,1 +1,14 @@
+import uvicorn
 import fastapi
+
+app = fastapi.FastAPI()
+
+
+@app.get('/')
+def calculate():
+    return {
+        'value': 4
+    }
+
+
+uvicorn.run(app, host="0.0.0.0", port=8888)
