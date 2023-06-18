4bfa57bfc7300443abf216d684fb58c6d9a20e90
Igor <igrik82@gmail.com>
2023-06-18 09:50:25 +0500

    storing posts in list

--- 02_python_api_development_youtube/01_proj/main.py
+++ 02_python_api_development_youtube/01_proj/main.py
@@ -1,5 +1,5 @@
 '''First project'''
-# import uvicorn
+from random import randint
 from typing import Optional
 from pydantic import BaseModel
 from fastapi import FastAPI
@@ -8,10 +8,25 @@ app = FastAPI()
 
 
 class Poster(BaseModel):
+    id: Optional[int] = None
     title: str
     content: str
     published: bool = True
     rating: Optional[int] = None
+
+
+# Storing data in lists
+my_posts = [{'title': 'title of post1', 'content': 'content post 1', 'id': 1},
+            {'title': 'title of post2', 'content': 'content post 2', 'id': 2},
+            {'title': 'title of post3', 'content': 'content post 3', 'id': 3}]
+
+
+def find_post(post_id):
+    for post in my_posts:
+        print(post['id'])
+        if post['id'] == post_id:
+            return post
+    return None
 
 
 @app.get('/')
@@ -19,11 +34,22 @@ def root():
     return {'message': 'Hello world!'}
 
 
-@app.post('/createpost')
+@app.post('/posts')
 def create_post(user_post: Poster):
-    print(user_post.published)
-    return {'Post': user_post}
+    post = user_post.dict()
+    if not post['id']:
+        post['id'] = randint(1, 1_000_000)
+    my_posts.append(post)
+    return {'Post': post}
 
 
-# if __name__ == "__main__":
-# uvicorn.run(app='main:app', host='0.0.0.0', port=8888, reload=True)
+@app.get('/posts/latest')
+def show_lat_post():
+    post = my_posts[len(my_posts) - 1]
+    return {'Post': post}
+
+
+@app.get('/posts/{post_id}')
+def show_post(post_id: int):
+    post = find_post(post_id)
+    return {'Post': post}
