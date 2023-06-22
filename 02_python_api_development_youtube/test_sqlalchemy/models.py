41523d14c06ee918161446b662a7f210e9ff6ea8
Igor <igrik82@gmail.com>
2023-06-22 17:55:49 +0500

    refactoring

--- 02_python_api_development_youtube/test_sqlalchemy/models.py
+++ 02_python_api_development_youtube/test_sqlalchemy/models.py
@@ -1,4 +1,4 @@
-'''Base model'''
+"""Base model"""
 
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
@@ -9,25 +9,26 @@ class Base(DeclarativeBase):
 
 
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     username: Mapped[str] = mapped_column(nullable=False)
     email_addr: Mapped[str]
-    post: Mapped[list['Post']] = relationship(back_populates='user')
+    post: Mapped[list["Post"]] = relationship(back_populates="user")
 
     def __repr__(self) -> str:
-        return f'<User username={self.username}'
+        return f"<User username={self.username}"
 
 
 class Post(Base):
-    __tablename__ = 'user_post'
+    __tablename__ = "user_post"
 
     user_id: Mapped[int] = mapped_column(
-        ForeignKey('users.id'), nullable=False)
+        ForeignKey("users.id"), nullable=False
+    )
     title: Mapped[str] = mapped_column(nullable=False)
     content: Mapped[str] = mapped_column(nullable=False)
     published: Mapped[bool] = mapped_column(default=True, nullable=False)
-    user: Mapped['User'] = relationship(back_populates='post')
+    user: Mapped["User"] = relationship(back_populates="post")
 
     def __repr__(self) -> str:
-        return f'<Post title={self.title} content={self.content}'
+        return f"<Post title={self.title} content={self.content}"
