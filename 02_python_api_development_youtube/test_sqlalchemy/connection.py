6a2891890dcf658a47e0207fc442879363f51d93
Igor <igrik82@gmail.com>
2023-06-22 17:41:19 +0500

    refactoring

--- 02_python_api_development_youtube/test_sqlalchemy/connection.py
+++ 02_python_api_development_youtube/test_sqlalchemy/connection.py
@@ -1,13 +1,12 @@
-'''Connection to Database'''
+"""Connection to Database"""
 
 from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
 
-DATABASE = 'test_post_user'
+DATABASE = "blog_posts"
 engine = create_engine(
-    f'postgresql+psycopg://fastapi:123456@192.168.88.226/{DATABASE}', echo=True)
+    f"postgresql+psycopg://fastapi:123456@192.168.88.226/{DATABASE}", echo=True
+)
 
 if not database_exists(engine.url):
     create_database(engine.url)
-
-print(database_exists(engine.url))
