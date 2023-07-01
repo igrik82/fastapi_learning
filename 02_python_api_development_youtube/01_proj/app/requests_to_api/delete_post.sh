192764b549d256eb8cd51a8b036195874aba5bd4
Igor <igrik82@gmail.com>
2023-07-01 15:45:37 +0500

    feat: Delete request

---
+++ 02_python_api_development_youtube/01_proj/app/requests_to_api/delete_post.sh
@@ -0,0 +1,26 @@
+
+#!/usr/bin/env bash
+
+if [[ -z "$1" || "$#" -ne 2 ]]; then
+    echo "Use option -n 1"
+    exit 1
+fi
+
+
+while getopts "n:" opt; do
+    case $opt in
+    n)
+        POST_NUM=${OPTARG};;
+    *)
+        echo "Invalid option! Use option -n 1"
+        exit 1;;
+
+
+    esac
+done
+
+TOKEN=$(cat token)
+
+curl -w '\n' -L -X DELETE http://192.168.88.226:8888/posts/${POST_NUM} \
+    -H 'accept: */*' \
+    -H "Authorization: Bearer ${TOKEN}" \
